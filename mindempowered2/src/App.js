19d6caa805089a69bfdd525e082dfcab0a72a88e
abhishek992001 <abhishekpuliyankalath@gmail.com>
2025-02-07 22:52:55 +0530

    added event page

---
+++ mindempowered2/src/App.js
@@ -0,0 +1,16 @@
+import Sidebar from "./components/Sidebar";
+import Events from "./components/Events";
+import './index.css';
+
+
+function App() {
+  return (
+    <div className="flex">
+       return <div className="bg-green-500 text-white p-5">Tailwind Works!</div>;
+      <Sidebar />
+      <Events />
+    </div>
+  );
+}
+
+export default App;
